c45c4e3a04fe50002b4f4883edfda8c7eaf97b62
Sajjad Hashemian <hi@hashemian.me>
2022-09-17 09:46:13 +0430

    change favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,31 @@
 import "../styles/globals.css";
 import "../styles/fonts.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Component {...pageProps} />
       <footer className="absolute bottom-0 py-4 w-full text-center border-on-background">
         <div className="flex justify-center items-center w-full text-sm">
